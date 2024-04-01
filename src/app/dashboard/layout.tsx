03e04a012fd8e6df425528bc189172d8f58164e0
aheadbio <mizukami@ahead-biocomputing.co.jp>
2024-04-01 15:29:18 +0900

    feat: サイドバーが動作するように修正 #29

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,19 +1,56 @@
 'use client'
+import React, { useState, useRef, useEffect } from 'react';
+
 export default function DashboardLayout({
     children,
   }: {
     children: React.ReactNode
   }) {
+    const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
+    const sidebarRef = useRef<HTMLDivElement>(null);
+    const toggleButtonRef = useRef<HTMLButtonElement>(null);
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
+    useEffect(() => {
+        // eventの型をMouseEventに指定
+        function handleClickOutside(event: MouseEvent) {
+          if (
+            sidebarRef.current && !sidebarRef.current.contains(event.target as Node) &&
+            toggleButtonRef.current && !toggleButtonRef.current.contains(event.target as Node)
+          ) {
+            setIsSidebarOpen(false);
+          }
+        }
+
+        // ドキュメントにクリックイベントリスナーを追加
+        document.addEventListener('mousedown', handleClickOutside);
+        // コンポーネントのクリーンアップ時にイベントリスナーを削除
+        return () => document.removeEventListener('mousedown', handleClickOutside);
+      }, []);
+
+
     return (
     <div className="bg-white">
-        <button data-drawer-target="sidebar-multi-level-sidebar" data-drawer-toggle="sidebar-multi-level-sidebar" aria-controls="sidebar-multi-level-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-            <span className="sr-only">Open sidebar</span>
+        <button
+            ref={toggleButtonRef}
+            onClick={toggleSidebar}
+            type="button"
+            className="md:hidden inline-flex items-center p-2 mt-2 ms-3text-sm text-gray-500 rounded-lg border border-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            >
             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
             </svg>
         </button>
 
-        <aside id="sidebar-multi-level-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+        <div
+            ref={sidebarRef}
+            className={`${
+                isSidebarOpen ? 'block' : 'hidden'
+              } md:block fixed top-0 left-0 z-40 w-64 h-screen transition-transform`}
+        >
         <div className="h-full px-3 py-4 overflow-y-auto bg-cyan-100 dark:bg-cyan-200">
             <ul className="space-y-2 font-medium">
                 <li>
@@ -84,7 +121,7 @@ export default function DashboardLayout({
                 </li>
             </ul>
         </div>
-        </aside>
+        </div>
         {children}
     </div>
     )
