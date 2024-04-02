4b2557a6839ffeef37b60aad83cd70de0fb12f69
aheadbio <mizukami@ahead-biocomputing.co.jp>
2024-04-02 14:20:02 +0900

    style:ダッシュボードのメインページのレイアウト調整 #30

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -38,8 +38,8 @@ export default function DashboardLayout({
             ref={toggleButtonRef}
             onClick={toggleSidebar}
             type="button"
-            className="md:hidden inline-flex items-center p-2 mt-2 ms-3text-sm text-gray-500 rounded-lg border border-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            >
+            className="fixed top-0 left-0 z-40 ml-2 p-2 mt-2 sm:hidden inline-flex items-center text-white rounded-lg border border-cyan-400 bg-cyan-400"
+        >
             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
             </svg>
@@ -49,7 +49,7 @@ export default function DashboardLayout({
             ref={sidebarRef}
             className={`${
                 isSidebarOpen ? 'block' : 'hidden'
-              } md:block fixed top-0 left-0 z-40 w-64 h-screen transition-transform`}
+              } sm:block fixed top-0 left-0 z-40 w-64 h-screen transition-transform`}
         >
         <div className="h-full px-3 py-4 overflow-y-auto bg-cyan-100 dark:bg-cyan-200">
             <ul className="space-y-2 font-medium">
